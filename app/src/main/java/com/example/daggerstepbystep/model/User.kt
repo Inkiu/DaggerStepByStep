11ec650b57f75bf0acd55a6c04c9087261869fb3
inkiu <hik0115@gmail.com>
2019-04-17 07:48:03 +0900

    user model added

---
+++ app/src/main/java/com/example/daggerstepbystep/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.daggerstepbystep.model
+
+data class User(
+    val id: Long,
+    val name: String,
+    val address: String,
+    val createdAt: String,
+    val updatedAt: String
+)
